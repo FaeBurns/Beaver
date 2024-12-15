3d29b5c707dde98cac765a65a1fbf24a760e3265
Fae <fae.burns0@gmail.com>
2024-12-15 19:37:00 +0000

    FFMpeg Windows integrated

--- Assets/Scripts/FFMpeg/Windows/WindowsFFMpegService.cs
+++ Assets/Scripts/FFMpeg/Windows/WindowsFFMpegService.cs
@@ -41,7 +41,7 @@ namespace FFMpeg.Windows
 
         private Stream OpenStreamServerPipe(IPEndPoint server, int width, int height, int frameRate)
         {
-            int bufferMult = 90;
+            int bufferMult = 5;
             int bufferSize = (width * height * 4) * bufferMult;
 
             Debug.Log($"Allocating pipe with buffer size of {bufferSize} | mult: {bufferMult}, based on size of {width * height * 4}");
@@ -54,14 +54,14 @@ namespace FFMpeg.Windows
                 PipeOptions.WriteThrough, bufferSize, bufferSize); // TODO: check buffer sizes
 
             string pipeString = @"\\.\pipe\" + pipeName;
-            string incomingFormat = "mpegts";
-            string protocol = "udp";
-            string outputFormat = $"-f rawvideo -pixel_format rgba -colorspace bt709 -vcodec rawvideo -r {frameRate} -video_size {width}x{height}";
-            string args = $"-f {incomingFormat} -probesize 8192 -fflags nobuffer -flags low_delay -i {protocol}://{server.Address}:{server.Port} {outputFormat} {pipeString} -y";
-            // string args = $"{formatArgs} -probesize 32 -fflags nobuffer -flags low_delay -i tcp://{server.Address}:{server.Port} -f rawvideo {videoArgs} pipe:1";
+            string protocol = "tcp";
+
+            string argsInput  = $"-f mpegts -probesize 8192 -fflags nobuffer -flags low_delay -i {protocol}://{server.Address}:{server.Port}";
+            string argsOutput = $"-f rawvideo -pix_fmt argb -colorspace bt709 -vcodec rawvideo -r {frameRate} -video_size {width}x{height} {pipeString} -y";
+            string args = argsInput + " " + argsOutput;
 
             Debug.Log($"executing ffmpeg with args {args}");
-            // WindowsFFMpegExec exec = new WindowsFFMpegExec(args);
+            WindowsFFMpegExec exec = new WindowsFFMpegExec(args);
             Debug.Log("Waiting for pipe connection...");
             inputPipe.WaitForConnection();
             Debug.Log("Pipe connection established.");
